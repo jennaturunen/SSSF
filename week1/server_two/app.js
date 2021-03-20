0ef2df4d792494a0a09ff3664dc0caf97c1ed751
jennaturunen <jenna.turunen2@gmail.com>
2021-03-20 16:56:40 +0200

    week 1 exercises

---
+++ week1/server_two/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World! Again and again');
+// });
+
+app.get('/', (req, res) => {
+  const uiContent = {
+    title: 'Example',
+    heading: 'Click on the cat',
+  };
+  res.render('index', uiContent);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get('/catinfo', (req, res) => {
+  const cat = {
+    name: 'Frank',
+    age: 6,
+    weight: 5,
+  };
+  res.json(cat);
+});
